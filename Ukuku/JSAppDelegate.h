bf0991d8dfe43604f5d642bdf98426b1d0736cf9
JuanSe Jativa <juansejativa@JuanSes-MacBook-Pro.local>
2014-09-03 10:29:44 -0500

    Create JSLogInViewController

--- Ukuku/JSAppDelegate.h
+++ Ukuku/JSAppDelegate.h
@@ -5,7 +5,6 @@
 //  Created by JuanSe Jativa on 02/09/14.
 //  Copyright (c) 2014 JuanSe Jativa. All rights reserved.
 //
-
 #import <UIKit/UIKit.h>
 
 @interface JSAppDelegate : UIResponder <UIApplicationDelegate>
---
+++ Ukuku/JSLogInVC.m
@@ -0,0 +1,38 @@
+//
+//  JSLogInVC.m
+//  Ukuku
+//
+//  Created by JuanSe Jativa on 03/09/14.
+//  Copyright (c) 2014 JuanSe Jativa. All rights reserved.
+//
+
+#import "JSLogInVC.h"
+
+@interface JSLogInVC ()
+
+@end
+
+@implementation JSLogInVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
