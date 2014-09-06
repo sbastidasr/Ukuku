f952ff9694c1f008287fc5239f030264152678ba
JuanSe Jativa <juanse417@hotmail.com>
2014-09-06 16:23:30 -0500

    User page with data from Facebook

--- Ukuku/JSProfileVC.m
+++ Ukuku/JSProfileVC.m
@@ -9,8 +9,13 @@
 #import "JSProfileVC.h"
 #import "JSAppDelegate.h"
 #import <Parse/Parse.h>
+#import <ParseFacebookUtils/PFFacebookUtils.h>
 
 @interface JSProfileVC ()
+
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *pictureImageView;
+@property (weak, nonatomic) IBOutlet UITextView *bioTextView;
 
 - (IBAction)logOutPressed:(id)sender;
 @end
@@ -21,7 +26,8 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
+        
+        
     }
     return self;
 }
@@ -29,8 +35,12 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    [self loadProfile];
+    [self configureNavigationBar];
+    self.title=@"Perfil";
     // Do any additional setup after loading the view.
 }
+
 
 - (void)didReceiveMemoryWarning
 {
@@ -38,6 +48,131 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)configureNavigationBar {
+    
+    
+    [self.navigationController.navigationBar setBackgroundImage:[UIImage new]
+                                                  forBarMetrics:UIBarMetricsDefault];
+    self.navigationController.navigationBar.shadowImage = [UIImage new];
+    self.navigationController.navigationBar.translucent = YES;
+    self.navigationController.view.backgroundColor = [UIColor clearColor];
+    
+    
+}
+
+-(void)loadProfile {
+
+    if([PFFacebookUtils isLinkedWithUser:[PFUser currentUser]]) {
+    
+        [self loadFromFacebook];
+    }
+    if([PFTwitterUtils isLinkedWithUser:[PFUser currentUser]]) {
+        
+        [self loadFromTwitter];
+    
+    }
+    
+
+}
+
+-(void)loadFromFacebook {
+
+    FBRequest *request = [FBRequest requestForMe];
+    [request startWithCompletionHandler:^(FBRequestConnection *connection, id result, NSError *error) {
+        // handle response
+        if (!error) {
+            // Parse the data received
+            NSDictionary *userData = (NSDictionary *)result;
+            
+            NSString *facebookID = userData[@"id"];
+            
+            
+            NSMutableDictionary *userProfile = [NSMutableDictionary dictionaryWithCapacity:3];
+            
+            if (facebookID) {
+                userProfile[@"facebookId"] = facebookID;
+            }
+            
+            NSString *name = userData[@"name"];
+            if (name) {
+                userProfile[@"name"] = name;
+            }
+            
+            NSString *location = userData[@"location"][@"name"];
+            if (location) {
+                userProfile[@"location"] = location;
+            }
+        
+            NSString *bio = userData[@"bio"];
+            if(bio) {
+            
+                userProfile[@"bio"] = bio;
+            }
+            
+            
+            userProfile[@"pictureURL"] = [NSString stringWithFormat:@"https://graph.facebook.com/%@/picture?type=large&return_ssl_resources=1", facebookID];
+            
+            [[PFUser currentUser] setObject:userProfile forKey:@"profile"];
+            [[PFUser currentUser] saveInBackground];
+            
+            [self updateProfileDataFacebook];
+        } else if ([[[[error userInfo] objectForKey:@"error"] objectForKey:@"type"]
+                    isEqualToString: @"OAuthException"]) { // Since the request failed, we can check if it was due to an invalid session
+            NSLog(@"The facebook session was invalidated");
+            //[self logoutButtonAction:nil];
+        } else {
+            NSLog(@"Some other error: %@", error);
+        }
+    }];
+
+
+}
+
+- (void)updateProfileDataFacebook {
+    
+    NSString *aboutMe =@"";
+    
+    NSString *location = [PFUser currentUser][@"profile"][@"location"];
+    if (location) {
+        aboutMe = [NSString stringWithFormat:@"%@\n", location];
+    }
+    
+    NSString *bio = [PFUser currentUser][@"profile"][@"bio"];
+    if (bio) {
+        aboutMe = [NSString stringWithFormat:@"%@, %@", aboutMe, bio];
+        self.bioTextView.text = bio;
+    }
+    
+    
+    // Set the name in the header view label
+    NSString *name = [PFUser currentUser][@"profile"][@"name"];
+    if (name) {
+        self.nameLabel.text = [NSString stringWithFormat:@"     %@", name];
+    }
+    
+    NSString *userProfilePhotoURLString = [PFUser currentUser][@"profile"][@"pictureURL"];
+    // Download the user's facebook profile picture
+    if (userProfilePhotoURLString) {
+        NSURL *pictureURL = [NSURL URLWithString:userProfilePhotoURLString];
+        NSURLRequest *urlRequest = [NSURLRequest requestWithURL:pictureURL];
+        
+        [NSURLConnection sendAsynchronousRequest:urlRequest
+                                           queue:[NSOperationQueue mainQueue]
+                               completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+                                   if (connectionError == nil && data != nil) {
+                                       self.pictureImageView.image = [UIImage imageWithData:data];
+                                       
+                                   } else {
+                                       NSLog(@"Failed to load profile photo.");
+                                   }
+                               }];
+    }
+}
+
+-(void)loadFromTwitter {
+
+
+}
 /*
 #pragma mark - Navigation
 
@@ -55,4 +190,25 @@
     JSAppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
     [appDelegate userDidLogOut];
 }
+
+#pragma Mark - OverrideMethods
+
+- (void)setTitle:(NSString *)title
+{
+    [super setTitle:title];
+    UILabel *titleView = (UILabel *)self.navigationItem.titleView;
+    if (!titleView) {
+        titleView = [[UILabel alloc] initWithFrame:CGRectZero];
+        titleView.backgroundColor = [UIColor clearColor];
+        titleView.font = [UIFont boldSystemFontOfSize:20.0];
+        titleView.shadowColor = [UIColor colorWithWhite:0.0 alpha:0.5];
+        
+        titleView.textColor = [UIColor whiteColor];
+        
+        self.navigationItem.titleView = titleView;
+    }
+    titleView.text = title;
+    [titleView sizeToFit];
+}
+
 @end
