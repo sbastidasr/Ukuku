a608dba7d3261afeb0a2914a2a3484f404cd5536
JuanSe Jativa <juanse417@hotmail.com>
2014-09-06 15:02:29 -0500

    Add JSProfileVC

---
+++ Ukuku/JSProfileVC.h
@@ -0,0 +1,13 @@
+//
+//  JSProfileVC.h
+//  Ukuku
+//
+//  Created by JuanSe Jativa on 06/09/14.
+//  Copyright (c) 2014 JuanSe Jativa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JSProfileVC : UIViewController
+
+@end
---
+++ Ukuku/JSProfileVC.m
@@ -0,0 +1,49 @@
+//
+//  JSProfileVC.m
+//  Ukuku
+//
+//  Created by JuanSe Jativa on 06/09/14.
+//  Copyright (c) 2014 JuanSe Jativa. All rights reserved.
+//
+
+#import "JSProfileVC.h"
+
+@interface JSProfileVC ()
+
+@end
+
+@implementation JSProfileVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
