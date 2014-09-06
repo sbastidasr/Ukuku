4595cc2fb2dc1ac49a84fe037fca53cba6eaedaa
JuanSe Jativa <juanse417@hotmail.com>
2014-09-06 13:25:51 -0500

    View Controller for SignUp

---
+++ Ukuku/JSSignUpVC.h
@@ -0,0 +1,13 @@
+//
+//  JSSignUpVC.h
+//  Ukuku
+//
+//  Created by JuanSe Jativa on 06/09/14.
+//  Copyright (c) 2014 JuanSe Jativa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JSSignUpVC : UIViewController
+
+@end
---
+++ Ukuku/JSSignUpVC.m
@@ -0,0 +1,38 @@
+//
+//  JSSignUpVC.m
+//  Ukuku
+//
+//  Created by JuanSe Jativa on 06/09/14.
+//  Copyright (c) 2014 JuanSe Jativa. All rights reserved.
+//
+
+#import "JSSignUpVC.h"
+
+@interface JSSignUpVC ()
+
+@end
+
+@implementation JSSignUpVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
