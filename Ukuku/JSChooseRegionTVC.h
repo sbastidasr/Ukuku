158df5670750bdd881a9e7e6a9177854acb39c18
JuanSe Jativa <juanse417@hotmail.com>
2014-09-10 11:28:03 -0500

    Add ChooseRegionTVC

---
+++ Ukuku/JSChooseRegionTVC.h
@@ -0,0 +1,13 @@
+//
+//  JSChooseRegionTVC.h
+//  Ukuku
+//
+//  Created by JuanSe Jativa on 10/09/14.
+//  Copyright (c) 2014 JuanSe Jativa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JSChooseRegionTVC : UITableViewController
+
+@end
