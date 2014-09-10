ccb3508216e5f3b4a0b31152aabbabeea0e3841c
JuanSe Jativa <juanse417@hotmail.com>
2014-09-10 11:55:47 -0500

    Add JSRiesgoTVC

---
+++ Ukuku/JSRiesgoTVC.h
@@ -0,0 +1,13 @@
+//
+//  JSRiesgoTVC.h
+//  Ukuku
+//
+//  Created by JuanSe Jativa on 10/09/14.
+//  Copyright (c) 2014 JuanSe Jativa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JSRiesgoTVC : UITableViewController
+
+@end
