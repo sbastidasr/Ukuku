cb15bb3902e20ca5a20a10b2d5b3eb18f08d81c2
Sebastian <sbastidasr@acc81153.ipt.aol.com>
2014-09-16 12:16:02 -0500

    Explorar Lista

---
+++ Ukuku/SBExplorarListaTVC.h
@@ -0,0 +1,13 @@
+//
+//  SBExplorarListaTVC.h
+//  Ukuku
+//
+//  Created by Sebastian Bastidas on 9/16/14.
+//  Copyright (c) 2014 JuanSe Jativa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SBExplorarListaTVC : UITableViewController
+
+@end
