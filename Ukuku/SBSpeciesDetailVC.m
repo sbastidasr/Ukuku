dcfd8cf723c1f3ffdb3803d308bec8ec74df30ce
Sebastian <sbastidasr@acc81153.ipt.aol.com>
2014-09-17 08:32:54 -0500

    Detail view Controller arreglar imagen pendiente

--- Ukuku/SBSpeciesDetailVC.m
+++ Ukuku/SBSpeciesDetailVC.m
@@ -10,6 +10,14 @@
 
 @interface SBSpeciesDetailVC ()
 @property (weak, nonatomic) IBOutlet UIScrollView *scroll;
+@property (weak, nonatomic) IBOutlet UIImageView *speciesDetailImage;
+@property (weak, nonatomic) IBOutlet UILabel *speciesDetailName;
+@property (weak, nonatomic) IBOutlet UILabel *speciesDetailTipo;
+@property (weak, nonatomic) IBOutlet UILabel *speciesDetailNombCient;
+@property (weak, nonatomic) IBOutlet UILabel *speciesDetailStatus;
+@property (weak, nonatomic) IBOutlet UILabel *speciesDetailRegion;
+@property (weak, nonatomic) IBOutlet UITextView *speciesDetailDescription;
+
 
 @end
 
@@ -30,9 +38,27 @@
     
     [self.scroll setScrollEnabled:YES];
     [self.scroll setContentSize:CGSizeMake(320, 835)];
-    NSLog(@"asd");
+    self.speciesDetailName.text = self.species[@"Nombre"];
+    self.speciesDetailTipo.text = self.species[@"Tipo"];
+    self.speciesDetailNombCient.text = self.species[@"NombreLatin"];
+    self.speciesDetailStatus.text = self.species[@"Status"];
+    self.speciesDetailRegion.text = self.species[@"Region"];
+    self.speciesDetailDescription.text = self.species[@"Descripcion"];
+    
+    
+    PFFile *theImage = self.species[@"foto"];
+    [theImage getDataInBackgroundWithBlock:^(NSData *data, NSError *error) {
+        NSData *imageData = data;
+        UIImage *image = [UIImage imageWithData:imageData];
+        self.speciesDetailImage.image=image;
+    }];
+    
+    //PFFile *thumbnail = self.species[@"foto"];
+    //self.speciesDetailImage.image=[UIImage imageNamed:@"placeholder.jpg"];
+    //self.speciesDetailImage.image = thumbnail;
+    
+    
 }
-
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
