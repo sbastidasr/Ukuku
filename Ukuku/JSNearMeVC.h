a9011b339be7b5cf68d9a4da5c5180df30402971
JuanSe Jativa <juanse417@hotmail.com>
2014-09-17 22:08:26 -0500

    add JSNearMeVC

---
+++ Ukuku/JSNearMeVC.h
@@ -0,0 +1,13 @@
+//
+//  JSNearMeVC.h
+//  Ukuku
+//
+//  Created by JuanSe Jativa on 17/09/14.
+//  Copyright (c) 2014 JuanSe Jativa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JSNearMeVC : UIViewController
+
+@end
