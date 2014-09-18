ee7851a4be448b723cf90f7c70eb2f10b4a0fcdf
JuanSe Jativa <juanse417@hotmail.com>
2014-09-18 12:17:15 -0500

    Add AllPhotoLocationVC and MapView

---
+++ Ukuku/JSAllPhotoLocationsVC.h
@@ -0,0 +1,13 @@
+//
+//  JSAllPhotoLocationsVC.h
+//  Ukuku
+//
+//  Created by JuanSe Jativa on 18/09/14.
+//  Copyright (c) 2014 JuanSe Jativa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JSAllPhotoLocationsVC : UIViewController
+
+@end
