8b1378f1f5202bd5c077bfc4e3e493a497857663
JuanSe Jativa <juanse417@hotmail.com>
2014-09-19 02:37:33 -0500

    add JSDetailPhoto and Button for Share and View

---
+++ Ukuku/JSDetailPhotoVC.h
@@ -0,0 +1,13 @@
+//
+//  JSDetailPhotoVC.h
+//  Ukuku
+//
+//  Created by JuanSe Jativa on 19/09/14.
+//  Copyright (c) 2014 JuanSe Jativa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JSDetailPhotoVC : UIViewController
+
+@end
---
+++ Ukuku/JSDetailPhotoVC.m
@@ -0,0 +1,49 @@
+//
+//  JSDetailPhotoVC.m
+//  Ukuku
+//
+//  Created by JuanSe Jativa on 19/09/14.
+//  Copyright (c) 2014 JuanSe Jativa. All rights reserved.
+//
+
+#import "JSDetailPhotoVC.h"
+
+@interface JSDetailPhotoVC ()
+
+@end
+
+@implementation JSDetailPhotoVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
